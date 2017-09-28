f1b84ba34352fc7f27f3756b1508f66e70e9142d
Jon Bradley <jon.bradley@nearmap.com>
2017-09-29 09:46:05 +1000

    parametise user restriction

--- lib/services/slack_service.ts
+++ lib/services/slack_service.ts
@@ -170,7 +170,8 @@ export class SlackService extends Service {
         reply(`Could not fetch your user info from Slack. ${error || ""}`)
       } else {
         const user = response.user
-        if (user.name !== "jon" && user.name !== "meagan" && user.name !== "less.formal" && user.name !== "harshils") {
+//        if (user.name !== "jon" && user.name !== "meagan" && user.name !== "less.formal" && user.name !== "harshils") {
+        if (!config.slackUsers.includes(user.name)) {
           reply(`Sorry @${user.name} you are not able to use this command.`)
         } else if (!config.enableGuestUsers && (user.is_restricted || user.is_ultra_restricted)) {
           reply(`Sorry @${user.name}, as a guest user you're not able to use this command.`)
