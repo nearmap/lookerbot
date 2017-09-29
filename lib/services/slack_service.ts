1383f56a7784f8e62e413435734a3463637c5b71
Jon Bradley <jon.bradley@nearmap.com>
2017-09-29 12:07:09 +1000

    parametise user restriction

--- lib/services/slack_service.ts
+++ lib/services/slack_service.ts
@@ -170,7 +170,7 @@ export class SlackService extends Service {
         reply(`Could not fetch your user info from Slack. ${error || ""}`)
       } else {
         const user = response.user
-        if (config.slackUsers != undefined && config.slackUsers.split(',').indexOf(user.name) >= 0) {
+        if (config.slackUsers != undefined && config.slackUsers.split(',').indexOf(user.name) > -1) {
           console.log("test" + config.slackUsers.split(',').indexOf(user.name))
           reply(`Sorry @${user.name} you are not able to use this command.`)
         } else if (!config.enableGuestUsers && (user.is_restricted || user.is_ultra_restricted)) {
